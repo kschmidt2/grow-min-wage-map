a6b4ed8bb9928f6b8ba95d016e9df59c9fd0f9ca
Kiersten <kiersten.schmidt@nbcuniversal.com>
2019-09-17 15:53:21 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -23,40 +23,419 @@ Highcharts.setOptions({
 document.addEventListener('DOMContentLoaded', function () {
     const myChart = Highcharts.chart('chart-container', {
         chart: {
-            type: 'bar',
-            styledMode: true,
+            type: 'tilemap',
+            inverted: true,
+            // styledMode: true,
             spacingBottom: 25,
-            spacingRight: 100
+            spacingRight: 100,
+            spacingLeft: -20
         }, 
         title: {
             text: null
         },
-        data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
-        },
-        // for bar charts only
         plotOptions: {
             series: {
-                groupPadding: 0.1
-            } 
+                enableMouseTracking: false,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.hc-a2}',
+                    color: '#000000',
+                    style: {
+                        textOutline: false
+                    }
+                }
+            }
         },
-        // for line charts only
-        // plotOptions: {
-        //     series: {
-        //         lineWidth: 1,
-        //         // clip: false,
-        //         marker: {
-        //             enabled: false,
-        //             symbol: 'circle',
-        //             fillColor: '#ffffff',
-        //             states: {
-        //                 hover: {
-        //                     fillColor: '#ffffff'
-        //                 }
-        //             }
-        //         }
-        //     }
-        // },
+        xAxis: {
+            visible: false
+        },
+           
+        yAxis: {
+            visible: false
+        },
+        colorAxis: {
+            dataClasses: [{
+                from: 0,
+                to: 7.25,
+                color: '#f5f5f5',
+                name: '$7.25'
+            }, {
+                from: 7.26,
+                to: 8.99,
+                color: '#bceaa3',
+                name: '$7.26-$8.99'
+            }, {
+                from: 9,
+                to: 10.99,
+                color: '#53b324',
+                name: '$9.00-$10.99'
+            }, {
+                from: 11,
+                color: '#15A77B',
+                name: '$11.00+'
+            }]
+        },
+        series: [{
+            name: '',
+            data: [{
+                'hc-a2': 'AL',
+                name: 'Alabama',
+                region: 'South',
+                x: 6,
+                y: 7,
+                value: 7.25
+            }, {
+                'hc-a2': 'AK',
+                name: 'Alaska',
+                region: 'West',
+                x: 0,
+                y: 0,
+                value: 9.89
+            }, {
+                'hc-a2': 'AZ',
+                name: 'Arizona',
+                region: 'West',
+                x: 5,
+                y: 3,
+                value: 11
+            }, {
+                'hc-a2': 'AR',
+                name: 'Arkansas',
+                region: 'South',
+                x: 5,
+                y: 6,
+                value: 9.25
+            }, {
+                'hc-a2': 'CA',
+                name: 'California',
+                region: 'West',
+                x: 5,
+                y: 2,
+                value: 12
+            }, {
+                'hc-a2': 'CO',
+                name: 'Colorado',
+                region: 'West',
+                x: 4,
+                y: 3,
+                value: 11.10
+            }, {
+                'hc-a2': 'CT',
+                name: 'Connecticut',
+                region: 'Northeast',
+                x: 3,
+                y: 11,
+                value: 10.10
+            }, {
+                'hc-a2': 'DE',
+                name: 'Delaware',
+                region: 'South',
+                x: 4,
+                y: 9,
+                value: 8.75
+            }, {
+                'hc-a2': 'DC',
+                name: 'District of Columbia',
+                region: 'South',
+                x: 4,
+                y: 10,
+                value: 14
+            }, {
+                'hc-a2': 'FL',
+                name: 'Florida',
+                region: 'South',
+                x: 8,
+                y: 8,
+                value: 8.46
+            }, {
+                'hc-a2': 'GA',
+                name: 'Georgia',
+                region: 'South',
+                x: 7,
+                y: 8,
+                value: 7.25
+            }, {
+                'hc-a2': 'HI',
+                name: 'Hawaii',
+                region: 'West',
+                x: 8,
+                y: 0,
+                value: 10.10
+            }, {
+                'hc-a2': 'ID',
+                name: 'Idaho',
+                region: 'West',
+                x: 3,
+                y: 2,
+                value: 7.25
+            }, {
+                'hc-a2': 'IL',
+                name: 'Illinois',
+                region: 'Midwest',
+                x: 3,
+                y: 6,
+                value: 8.25
+            }, {
+                'hc-a2': 'IN',
+                name: 'Indiana',
+                region: 'Midwest',
+                x: 3,
+                y: 7,
+                value: 7.25
+            }, {
+                'hc-a2': 'IA',
+                name: 'Iowa',
+                region: 'Midwest',
+                x: 3,
+                y: 5,
+                value: 7.25
+            }, {
+                'hc-a2': 'KS',
+                name: 'Kansas',
+                region: 'Midwest',
+                x: 5,
+                y: 5,
+                value: 7.25
+            }, {
+                'hc-a2': 'KY',
+                name: 'Kentucky',
+                region: 'South',
+                x: 4,
+                y: 6,
+                value: 7.25
+            }, {
+                'hc-a2': 'LA',
+                name: 'Louisiana',
+                region: 'South',
+                x: 6,
+                y: 5,
+                value: 7.25
+            }, {
+                'hc-a2': 'ME',
+                name: 'Maine',
+                region: 'Northeast',
+                x: 0,
+                y: 11,
+                value: 11
+            }, {
+                'hc-a2': 'MD',
+                name: 'Maryland',
+                region: 'South',
+                x: 4,
+                y: 8,
+                value: 10.10
+            }, {
+                'hc-a2': 'MA',
+                name: 'Massachusetts',
+                region: 'Northeast',
+                x: 2,
+                y: 10,
+                value: 12
+            }, {
+                'hc-a2': 'MI',
+                name: 'Michigan',
+                region: 'Midwest',
+                x: 2,
+                y: 7,
+                value: 9.45
+            }, {
+                'hc-a2': 'MN',
+                name: 'Minnesota',
+                region: 'Midwest',
+                x: 2,
+                y: 4,
+                value: 9.86
+            }, {
+                'hc-a2': 'MS',
+                name: 'Mississippi',
+                region: 'South',
+                x: 6,
+                y: 6,
+                value: 7.25
+            }, {
+                'hc-a2': 'MO',
+                name: 'Missouri',
+                region: 'Midwest',
+                x: 4,
+                y: 5,
+                value: 8.6
+            }, {
+                'hc-a2': 'MT',
+                name: 'Montana',
+                region: 'West',
+                x: 2,
+                y: 2,
+                value: 8.50
+            }, {
+                'hc-a2': 'NE',
+                name: 'Nebraska',
+                region: 'Midwest',
+                x: 4,
+                y: 4,
+                value: 9
+            }, {
+                'hc-a2': 'NV',
+                name: 'Nevada',
+                region: 'West',
+                x: 4,
+                y: 2,
+                value: 8.25
+            }, {
+                'hc-a2': 'NH',
+                name: 'New Hampshire',
+                region: 'Northeast',
+                x: 1,
+                y: 11,
+                value: 7.25
+            }, {
+                'hc-a2': 'NJ',
+                name: 'New Jersey',
+                region: 'Northeast',
+                x: 3,
+                y: 10,
+                value: 10
+            }, {
+                'hc-a2': 'NM',
+                name: 'New Mexico',
+                region: 'West',
+                x: 6,
+                y: 3,
+                value: 7.50
+            }, {
+                'hc-a2': 'NY',
+                name: 'New York',
+                region: 'Northeast',
+                x: 2,
+                y: 9,
+                value: 11.10
+            }, {
+                'hc-a2': 'NC',
+                name: 'North Carolina',
+                region: 'South',
+                x: 5,
+                y: 9,
+                value: 7.25
+            }, {
+                'hc-a2': 'ND',
+                name: 'North Dakota',
+                region: 'Midwest',
+                x: 2,
+                y: 3,
+                value: 7.25
+            }, {
+                'hc-a2': 'OH',
+                name: 'Ohio',
+                region: 'Midwest',
+                x: 3,
+                y: 8,
+                value: 8.55
+            }, {
+                'hc-a2': 'OK',
+                name: 'Oklahoma',
+                region: 'South',
+                x: 6,
+                y: 4,
+                value: 7.25
+            }, {
+                'hc-a2': 'OR',
+                name: 'Oregon',
+                region: 'West',
+                x: 4,
+                y: 1,
+                value: 11.25
+            }, {
+                'hc-a2': 'PA',
+                name: 'Pennsylvania',
+                region: 'Northeast',
+                x: 3,
+                y: 9,
+                value: 7.25
+            }, {
+                'hc-a2': 'RI',
+                name: 'Rhode Island',
+                region: 'Northeast',
+                x: 2,
+                y: 11,
+                value: 10.50
+            }, {
+                'hc-a2': 'SC',
+                name: 'South Carolina',
+                region: 'South',
+                x: 6,
+                y: 8,
+                value: 7.25
+            }, {
+                'hc-a2': 'SD',
+                name: 'South Dakota',
+                region: 'Midwest',
+                x: 3,
+                y: 4,
+                value: 9.10
+            }, {
+                'hc-a2': 'TN',
+                name: 'Tennessee',
+                region: 'South',
+                x: 5,
+                y: 7,
+                value: 7.25
+            }, {
+                'hc-a2': 'TX',
+                name: 'Texas',
+                region: 'South',
+                x: 7,
+                y: 4,
+                value: 7.25
+            }, {
+                'hc-a2': 'UT',
+                name: 'Utah',
+                region: 'West',
+                x: 5,
+                y: 4,
+                value: 7.25
+            }, {
+                'hc-a2': 'VT',
+                name: 'Vermont',
+                region: 'Northeast',
+                x: 1,
+                y: 10,
+                value: 10.77
+            }, {
+                'hc-a2': 'VA',
+                name: 'Virginia',
+                region: 'South',
+                x: 5,
+                y: 8,
+                value: 7.25
+            }, {
+                'hc-a2': 'WA',
+                name: 'Washington',
+                region: 'West',
+                x: 2,
+                y: 1,
+                value: 12
+            }, {
+                'hc-a2': 'WV',
+                name: 'West Virginia',
+                region: 'South',
+                x: 4,
+                y: 7,
+                value: 8.75
+            }, {
+                'hc-a2': 'WI',
+                name: 'Wisconsin',
+                region: 'Midwest',
+                x: 2,
+                y: 5,
+                value: 7.25
+            }, {
+                'hc-a2': 'WY',
+                name: 'Wyoming',
+                region: 'West',
+                x: 3,
+                y: 3,
+                value: 7.25
+            }]
+        }],
         legend: {
             align: 'right',
             symbolRadius: 0,
@@ -64,27 +443,11 @@ document.addEventListener('DOMContentLoaded', function () {
             x: 10,
             itemMarginTop: -10
         },
-        xAxis: {
-            labels: {
-                style: {
-                    whiteSpace: 'nowrap'
-                }
-            },
-            tickLength: 5
-        },
-        yAxis: {
-            title: false,
-            labels: {
-                useHTML: true,
-                overflow: 'allow'
-            }
-        },
         credits: {
             enabled: false
         },
         tooltip: {
-            shadow: false,
-            padding: 10
+            enabled: false
         },
         responsive: {
             rules: [{
@@ -93,11 +456,12 @@ document.addEventListener('DOMContentLoaded', function () {
               },
               chartOptions: {
                 chart: {
-                  spacingRight: 10
+                  spacingRight: -35,
+                  spacingLeft: -10
                 },
                 legend: {
                     align: 'left',
-                    x: -18
+                    itemMarginTop: 0
                 },
                 tooltip: {
                     enabled: false
